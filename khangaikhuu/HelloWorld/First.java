0a2ba3893cce6d8b7000fdb37a411868355608fe
G12 <g12@asu.local>
2019-04-11 13:13:28 +0800

    merging

---
+++ khangaikhuu/HelloWorld/First.java
@@ -0,0 +1,46 @@
+
+/**
+ * Write a description of class First here.
+ *
+ * @author (Myanganbayar Nyamdavaa)
+ * @version (a version number or a date)
+ */
+public class First
+{
+    // instance variables - replace the example below with your own
+    private int x = 100;
+    private double y = 10.0;
+    private float z = 20.0f;
+    private String text = "Text";
+    private boolean isTrue = false;
+    private char c = 'c';
+    private long l = 123123123;
+
+    /**
+     * Constructor for objects of class First
+     */
+    public First()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    
+    public First(int a, double e)
+    {
+        x = x + a;
+        y = y + e;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
+
---
+++ khangaikhuu/HelloWorld/Method.java
@@ -0,0 +1,16 @@
+
+public class Method
+{
+    public int weight = 185;
+    public int height = 85;
+    
+    public int getHeight()
+    {
+        return height;
+        
+    }
+    public int getWeight(int addWeight)
+    {
+        return weight + addWeight;
+    }
+}
