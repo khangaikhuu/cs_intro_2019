17cdfb8b0ca163384dde73b5922893c6a1994abb
G12 <g12@asu.local>
2019-04-11 13:47:57 +0800

    ari

---
+++ Ariunsuren/4.10_Aiunsuren_ClassworkJava in BlueJ/Classwork1/PrintMyName.java
@@ -0,0 +1,18 @@
+
+
+public class PrintMyName
+{
+  public void printMyName()
+  {
+      System.out.println("Ariunsuren");
+    }
+    public void setName (String)
+    {
+        
+    }
+    public getName ()
+    {
+        return name; 
+    }
+} 
+
