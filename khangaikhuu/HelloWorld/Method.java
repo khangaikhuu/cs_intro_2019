a7928328413dc7e9b886095577a39f92d6be15fc
G12 <g12@asu.local>
2019-04-11 12:45:19 +0800

    classworkKhulan

---
+++ khangaikhuu/HelloWorld/Method.java
@@ -0,0 +1,19 @@
+
+
+public class Method
+{
+   private int height = 185;
+   private int weight = 85;
+   
+  private int getHeight()
+   {
+       return height;
+       
+   
+    }
+   public int getWeight(int a)
+    {
+        return weight + a;
+    }
+}
+
---
+++ khangaikhuu/HelloWorld/first.java
@@ -0,0 +1,42 @@
+
+
+public class first
+{
+    // instance variables - replace the example below with your own
+    private int x = 100;
+    private double y = 10.0;
+    private float z = 20.0f;
+    private String text = "Txt";
+    private boolean isTrue = false;
+    private char c = 'c';
+    private long l = 145;
+    
+
+    /**
+     * Constructor for objects of class first
+     */
+    public first()
+    {
+        // initialise instance variables
+        x = 10;
+      
+    }
+public first(int a, double e)
+{ 
+    x = x + a;
+    y = y + e;
+    
+}
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y) 
+    {
+        // put your code here
+        return x + y;
+    }
+}
