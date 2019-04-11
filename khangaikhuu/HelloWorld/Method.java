5529a59f436c10c2ff15d8e3fa44f030c3d47b7a
G9 <g9@asu.local>
2019-04-11 12:43:33 +0800

    JAVA

---
+++ khangaikhuu/HelloWorld/Method.java
@@ -0,0 +1,31 @@
+
+
+public class Method
+{
+    private int height = 185;
+    private int weight = 85;
+    
+public int getHeight()
+{
+    return height;
+   }
+
+   public int getWeight()
+   {
+       return weight;
+    }
+    
+    public int getWeight(int addWeight)
+    { 
+        return weight + addWeight;
+    }
+}
+    
+        
+    
+    
+    
+    
+    
+      
+    
