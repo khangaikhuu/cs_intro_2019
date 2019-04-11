7f77176cb0008998be7f856c56c0865ceb882bc4
G12 <g12@asu.local>
2019-04-11 12:49:51 +0800

    Bilegt

---
+++ khangaikhuu/HelloWorld/Method.java
@@ -0,0 +1,17 @@
+
+
+public class Method
+{
+    private int height = 185;
+    private int weight = 85;
+    
+    public int getHeight()
+    {
+     return height;   
+    }
+    
+    public int getWeight(int a)
+{
+    return weight + a;
+}
+}
