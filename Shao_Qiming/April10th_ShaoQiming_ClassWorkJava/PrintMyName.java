aa4a721f91998fe18dc37333256d75a31dc91a59
G12 <g12@asu.local>
2019-04-11 14:00:21 +0800

    merge

---
+++ Shao_Qiming/April10th_ShaoQiming_ClassWorkJava/PrintMyName.java
@@ -0,0 +1,14 @@
+public class PrintMyName
+{
+    private String a = "";
+    
+    public String setName()
+    {
+        
+    }
+    public String GetName()
+    {
+       return a;
+    }
+    
+}
