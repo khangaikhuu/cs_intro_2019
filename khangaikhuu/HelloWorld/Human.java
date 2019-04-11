a5822da622741e1c353dd11f9d77b2d3715d62f6
G12 <g12@asu.local>
2019-04-11 12:55:47 +0800

    tg

---
+++ khangaikhuu/HelloWorld/Human.java
@@ -0,0 +1,22 @@
+
+public class Human
+{
+   private int height;
+   private int weight;
+   private boolean isMan;
+   private String educated;
+  
+   
+   public Human (int h, int w)
+   {
+     height = height + h;
+     weight = weight = w;
+     isMan = false;
+     educated = "Not educated";
+    }
+    // method
+    public int getHeight()
+    {
+        return height;
+    }
+}
