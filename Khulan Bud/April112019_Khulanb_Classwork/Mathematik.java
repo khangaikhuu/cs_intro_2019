cd7e75073d76d1c24bccb553cda9199050a95dae
G12 <g12@asu.local>
2019-04-11 13:45:50 +0800

    classworkapril11

---
+++ Khulan Bud/April112019_Khulanb_Classwork/Mathematik.java
@@ -0,0 +1,25 @@
+
+
+public class Mathematik
+{
+    private int x;
+    private int y;
+    private int z;
+    public int calculateAddition(int a, int b)
+    {
+        return a + b;
+    }
+    public int calculateSubtraction(int a, int b)
+    {
+        return a - b;
+    }
+    public double calculateDivision(int a, int b)
+    {
+        return a / b;
+    }
+    public int calculateMultiplication(int a, int b)
+    {
+        return a * b;
+   
+    }
+}
