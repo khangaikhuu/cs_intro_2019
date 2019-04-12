15b76ea49fc912a617046cbdd214e3b284ca4cf7
G12 <g12@asu.local>
2019-04-12 12:38:25 +0800

    hannah

---
+++ Hannah Cho/Classwork_April 12, 2019_Hannah Cho/JavaOperations.java
@@ -0,0 +1,35 @@
+
+
+public class JavaOperations
+{
+    private int a = 2;
+    private int b = 14;
+    private int c = 6; 
+    
+    public int getA()
+    {
+        return a;
+    }
+    public int getB()
+    {
+        return b;
+    }
+    public int getC()
+    {
+        return c;
+    }
+    
+    public void setA(int pA)
+    {
+        a = pA;
+    }
+    public void setB(int pB)
+    {
+        b = pB;
+    }
+    public void setC(int pC)
+    {
+        c = pC;
+    }
+    
+}
---
+++ Hannah Cho/Classwork_April 12, 2019_Hannah Cho/TestJavaOperations.java
@@ -0,0 +1,22 @@
+
+
+public class TestJavaOperations
+{
+    private int a = 1;
+    private int b = 5;
+    private int c = 7;
+    
+    public int getA()
+    {
+        return a;
+    }
+    public int getB()
+    {
+        return b;
+    }
+    public int getC()
+    {
+        return c;
+    }
+    
+}
