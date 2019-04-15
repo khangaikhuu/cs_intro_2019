e9c119b99a60aebeb9d5c82321e441bfc59143f1
G12 <g12@asu.local>
2019-04-15 13:41:46 +0800

    15.04.2019_Sung Min_ClassworkJAva BlueJ

---
+++ Sung Min/15.04.2019_Sung Min_ClassworkJava in BlueJ/15.04.2019_Sung Min_Classwork 2 Java BlueJ/CalculatorTest.java
@@ -0,0 +1,21 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest
+{
+    @Test
+    public void testMyMethod()
+    {
+        Calculator c1 = new Calculator();
+        assertEquals(10, c1.add(5,5));
+        assertEquals(0, c1.subtract(4,4));
+        assertEquals(16, c1.multiply(4,4));
+        assertEquals(2, c1.division(4,2));
+        assertEquals(0, c1.modulo(2,2));
+    }
+    
+}
