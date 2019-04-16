c9e71e463ea07c189873141a3c972995ba1c35b3
G12 <g12@asu.local>
2019-04-16 14:05:22 +0800

    indra classwork04122019

---
+++ Indra/Classwork04122019_Indra Erdenebulgan/JavaOperations.java
@@ -0,0 +1,69 @@
+
+public class JavaOperations
+{
+    private int a = 34;
+    private double b = 2.5f;
+    private float c = 3.4f;
+    private long d = 23;
+    private char e = 8;
+    private boolean f = true;
+    private String myName = "Indra";
+    
+    public int getA()
+    {
+        return a;
+    }
+    public double getB()
+    {
+        return b;
+    }
+    public float getC()
+    {
+        return c;
+    }
+    public long getD()
+    {
+        return d;
+    }
+    public char getE()
+    {
+        return e;
+    }
+    public boolean getF()
+    {
+        return f;
+    }
+    public String getName()
+    {
+        return myName;
+    }
+    public void setA(int pA)
+    {
+        a = pA;
+    }
+    public void setB(double pA)
+    {
+        b = pA;
+    }
+    public void setC(float pA)
+    {
+        c = pA;
+    }
+    public void setD(long pA)
+    {
+        d = pA;
+    }
+    public void setE(char pA)
+    {
+        e = pA;
+    }
+    public void setF(boolean pA)
+    {
+        f = pA;
+    }
+    public void setC(String pA)
+    {
+        myName = pA;
+    }
+    
+}
