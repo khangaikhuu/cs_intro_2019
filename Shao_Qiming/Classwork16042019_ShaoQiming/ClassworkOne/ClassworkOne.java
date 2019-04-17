96b8053f630716bcbe71e15b6bd45c54efe3094b
G12 <g12@asu.local>
2019-04-17 13:44:38 +0800

    question 1 finished and tested

---
+++ Shao_Qiming/Classwork16042019_ShaoQiming/ClassworkOne/ClassworkOne.java
@@ -0,0 +1,32 @@
+
+public class ClassworkOne
+{
+   private int i;
+   private boolean b;
+   private String s;
+   public ClassworkOne(int n, boolean o, String t)
+   {
+       i = n;
+       b = o;
+       s = t;
+       
+   }
+   public void setI(int n, boolean o, String t)
+   {
+       i = n;
+       b = o;
+       s = t;
+    }
+   public int getI(int i)
+   {
+       return i;
+   }
+   public boolean getB(boolean b)
+   {
+       return b;
+   }
+   public String getS(String s)
+   {
+       return s;
+   }
+}
