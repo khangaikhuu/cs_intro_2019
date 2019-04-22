fb9df476bd8866be8d3c7006046b0b36504d3887
G8 <G8@asu.local>
2019-04-22 12:59:42 +0800

    Classwork 04.17.2019_Myanganbayar

---
+++ Myanganbayar Naransukh/Classwork 04.17.2019_Myanganbayar/Classwork04.17.2019/ClassworkThree.java
@@ -0,0 +1,17 @@
+
+public class ClassworkThree
+{
+    private int a = 0;
+    public int getI(int a)
+    {
+        return a;
+    }
+    public int loopI(int l)
+    {
+        for (int i = 20; i <= 20; i++)
+        {
+            a = a + 20;
+        }
+        return a;
+    }
+}
---
+++ Myanganbayar Naransukh/Classwork 04.17.2019_Myanganbayar/Classwork04.17.2019/ClassworkThreeTest.java
@@ -0,0 +1,17 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClassworkThreeTest
+{
+    @Test 
+    public void ClassworkThree()
+    {
+        ClassworkThree c1 = new ClassworkThree();
+        assertEquals(20, c1.getI(20));
+        assertEquals(20, c1.loopI(20));
+    }
+}
