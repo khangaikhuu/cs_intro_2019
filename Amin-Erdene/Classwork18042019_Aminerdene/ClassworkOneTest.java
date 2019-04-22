e66544705158b93b5002b37026a0c94b1309d31f
G12 <g12@asu.local>
2019-04-22 13:40:55 +0800

    amina's work

---
+++ Amin-Erdene/Classwork18042019_Aminerdene/ClassworkOneTest.java
@@ -0,0 +1,17 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class ClassworkOneTest
+{
+    @Test
+    public void testOne()
+    {
+        ClassworkOne c1 = new ClassworkOne (1);
+        assertEquals (1, c1.getA());
+        assertEquals (1, c1.getLoop(1));
+        assertEquals (4, c1.getLoop(2));
+    }
+}
