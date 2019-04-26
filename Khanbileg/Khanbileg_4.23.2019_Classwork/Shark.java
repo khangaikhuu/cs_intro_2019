08e953b29f24965f9f7c3461fe9ac5854aa13281
G12 <g12@asu.local>
2019-04-26 13:03:31 +0800

    Khanbileg_4.23.2019_Classwork

---
+++ Khanbileg/Khanbileg_4.23.2019_Classwork/Shark.java
@@ -0,0 +1,15 @@
+
+public class Shark
+{
+    private boolean is = true;
+    private int myInteger = 2;
+    
+    public boolean JahsehIsDead()
+    {
+        return !is;
+    }
+    public int Bullets( int myInteger)
+    {
+        return myInteger + 100; 
+    }
+}
