45ac9c8e488ee5ab0ea3f9f9c78fc6006f68b279
G12 <g12@asu.local>
2019-04-29 13:08:23 +0800

    asdf

---
+++ Shao_Qiming/Classwrok23042019_ShaoQiming/Shark/ClassworkOne.java
@@ -0,0 +1,27 @@
+
+public class ClassworkOne
+{
+    private int a;
+    private String b;
+    public ClassworkOne(int par1, String par2)
+    {
+         a = par1;
+         b = par2;
+    }
+    public int getA(int a)
+    {
+        return a;
+    }
+    public String getB(String b)
+    {
+        return b;
+    }
+    public void setC(int c)
+    {
+        a = c;
+    }
+    public void setD(String d)
+    {
+        b = d;
+    }
+}
