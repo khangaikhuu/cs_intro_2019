6d7ec99869c00750b252be909797b576f8983541
G12 <g12@asu.local>
2019-04-29 13:07:44 +0800

    classwork

---
+++ Nomin-Erdene/Classwork26_April262019_nominerdene/April262019_Classwork26_nominerdene/PerimeterOfRectangle.java
@@ -0,0 +1,10 @@
+
+public class PerimeterOfRectangle
+{
+    public int height;
+   public int width; 
+   public int PerimeterOfRectangle(int height, int width) 
+   { 
+       return 2 * (height + width); 
+    } 
+}
