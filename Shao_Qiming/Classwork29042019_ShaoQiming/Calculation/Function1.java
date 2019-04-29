e5656c300d81cbaa8cdec606152e048efde83dc9
charlietheuni <shao_qiming@outlook.com>
2019-04-29 21:26:41 +0800

    Extra classwork finished

---
+++ Shao_Qiming/Classwork29042019_ShaoQiming/Calculation/Function1.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class Function1
+{
+    private double x;
+    private double y;
+    public double f(double x, double y)
+    {
+        return Math.pow(x, 2) + Math.pow(y, 2) - 2 * x * y;
+        
+    }
+}
