6489404f483a25f24f141d2db626a398df1939ad
G12 <g12@asu.local>
2019-05-01 13:57:29 +0800

    lol

---
+++ Yalguunaa Ganbaatar/Classwork01052019_Yalguunaa Ganbaatar/Classwork/Equal.java
@@ -0,0 +1,15 @@
+
+public class Equal
+{
+   public boolean checkEqual(int a, int b)
+   {
+       if (a = b)
+       {
+           return true;
+        }
+       else
+       {
+           return false;
+        }
+}
+} 
---
+++ Yalguunaa Ganbaatar/Classwork01052019_Yalguunaa Ganbaatar/Classwork/xyCalcTest.java
@@ -0,0 +1,19 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class xyCalcTest
+{
+   @Test
+   public void testXyCalc()
+   {
+       xyCalc q1 = new xyCalc();
+       assertEquals(8,q1.calcF(2,2));
+       assertEquals(13,q1.calcF(2,3));
+       assertEquals(17,q1.calcF(1,4));
+    }
+}
