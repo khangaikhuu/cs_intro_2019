00774476c23afd87bd4eab0af9bb188f664a28e0
Apple <apple@Apples-MacBook-Air.local>
2019-05-01 23:44:43 +0900

    lol

--- Yalguunaa Ganbaatar/Classwork01052019_Yalguunaa Ganbaatar/Classwork/LessThan100Test.java
+++ Yalguunaa Ganbaatar/Classwork01052019_Yalguunaa Ganbaatar/Classwork/LessThan100Test.java
@@ -9,4 +9,10 @@ public class LessThan100Test
 {
     @Test
     public void testLessThan100()
+    {
+       LessThan100 q1 = new LessThan100();
+       assertEquals(true,q1.check100(12,1));
+       assertEquals(false,q1.check100(101,1));
+       assertEquals(true,q1.check100(7,3));
+    }
 }
