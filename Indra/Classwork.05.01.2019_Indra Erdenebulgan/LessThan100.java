4153fe63e0d32ca6b86644c14832c6b1e5361e34
G12 <g12@asu.local>
2019-05-02 13:40:33 +0800

    indra classwork

--- Indra/Classwork.05.01.2019_Indra Erdenebulgan/LessThan100.java
+++ Indra/Classwork.05.01.2019_Indra Erdenebulgan/LessThan100.java
@@ -4,24 +4,23 @@ public class LessThan100
    public int a; 
    public int b; 
    public boolean LessThan100(int a, int b) 
-{ 
-  if(a  < 100) 
-  { 
-      return true; 
-    } 
-  if(b < 100) 
-  { 
-        return true;
-  } 
+   { 
+       if(a  < 100) 
+       { 
+           return true; 
+        } 
+        if(b < 100) 
+        { 
+            return true;
+        } 
     
-    else 
-    { 
-        return false; 
+        else 
+        { 
+            return false; 
+        } 
+
+
     } 
-
-
- 
-} 
 } 
 
       
