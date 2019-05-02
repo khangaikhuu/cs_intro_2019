d0b4f05354adf51d0488b8fdb3e88290d41455b6
G12 <g12@asu.local>
2019-05-02 13:52:53 +0800

    merging

---
+++ Radnaasedi/01052019_Radnaasedi_classwork/Classwork18042019_Chingun/04_18_2019_Chingun_Classwork18/ClassworkOne.java
@@ -0,0 +1,23 @@
+
+public class ClassworkOne
+{
+   private int murda = 1; 
+   
+   public ClassworkOne(int a) 
+   { 
+       murda = a; 
+    } 
+   public int getCide(int murda) 
+   { 
+       return murda; 
+    } 
+   public int multiply(int par) 
+   { 
+       for (int i = 1; i <= par; i++) 
+       { 
+           murda = murda * murda; 
+        } 
+        return murda; 
+    } 
+   
+}
---
+++ Radnaasedi/01052019_Radnaasedi_classwork/Classwork26042019_Chingun/04_26_2019_Chingun_Battogtokh_Classwork26/AddNumberstest.java
@@ -0,0 +1,19 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class AddNumberstest
+{ 
+    @Test 
+    public void testAddNumbers() 
+    { 
+       AddNumbers c4 = new AddNumbers(); 
+       assertEquals(10, c4.AddNumbers(2, 3, 5)); 
+       assertEquals(14, c4.AddNumbers(6, 7, 1)); 
+       assertEquals(30, c4.AddNumbers(10,12, 8)); 
+    } 
+}
