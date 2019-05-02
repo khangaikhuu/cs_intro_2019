1f72a3de5e869ebb53276b6aa3ebaa0d2b178701
G12 <g12@asu.local>
2019-05-02 13:36:34 +0800

    indra

---
+++ Indra/Classwork.05.01.2019_Indra Erdenebulgan/Fxyz.java
@@ -0,0 +1,10 @@
+public class Fxyz
+{ 
+    public boolean X; 
+    public boolean Y; 
+    public boolean Z; 
+    public boolean Fxyz(boolean X, boolean Y, boolean Z) 
+    { 
+        return  X && Y && Z || !(X) && !(Y);
+    }
+}
