c81cbdbf8297b86158b689dd2cf50dde5e685975
G12 <g12@asu.local>
2019-05-02 13:48:18 +0800

    merging

--- Khanbileg/Khanbileg_5.01.2019_Classwork/VolumeCylinder.java
+++ Khanbileg/Khanbileg_5.01.2019_Classwork/VolumeCylinder.java
@@ -4,7 +4,7 @@ public class VolumeCylinder
     public int h; 
     public int r;
     
-    private double VolumeCylinder( int h, int r)
+    public double VolumeCylinder( int h, int r)
     {
         return r * r * h * 3.14;
     }
--- Khanbileg/Khanbileg_5.01.2019_Classwork/VolumeCylinderTest.java
+++ Khanbileg/Khanbileg_5.01.2019_Classwork/VolumeCylinderTest.java
@@ -8,7 +8,12 @@ import org.junit.Test;
 
 public class VolumeCylinderTest
 {
-    @test 
-    public void VolumeCylinderTest
-    
+    @Test 
+    public void VolumeCylinderTest()
+    {
+    VolumeCylinder v1 = new VolumeCylinder();
+    assertEquals(615.44, v1.VolumeCylinder(4, 7),1); 
+    assertEquals(62.800000000000004, v1.VolumeCylinder(5, 2),1);
+    assertEquals(1808.64, v1.VolumeCylinder(9, 8),1);
+   }
 }
