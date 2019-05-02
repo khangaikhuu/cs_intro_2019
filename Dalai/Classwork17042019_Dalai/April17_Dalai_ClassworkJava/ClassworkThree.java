a8dc7dc4b84cd1d875004102a3ba0a469639073f
Dalai Ankhbayar <Dalai@Khs-MacBook-Pro-2.local>
2019-05-02 15:33:40 +0800

    something

---
+++ Dalai/Classwork17042019_Dalai/April17_Dalai_ClassworkJava/ClassworkThree.java
@@ -0,0 +1,19 @@
+public class ClassworkThree
+{
+    private int Butt = 8;
+    
+    public ClassworkThree(int par1)
+    {
+        Butt = par1;
+    }
+    
+    public int getButt()
+    {
+        return Butt;
+    }
+    
+    public int loop()
+    {
+        for(int i=1; i<= 10; i++)
+    }
+}
