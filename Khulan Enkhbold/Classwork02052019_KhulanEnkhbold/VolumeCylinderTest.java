d23d68c0dcd8f21f9645c31270d6a9645b1fa492
G8 <G8@asu.local>
2019-05-03 12:53:22 +0800

    Classwork02052019_KhulanEnkhbold

--- Khulan Enkhbold/Classwork02052019_KhulanEnkhbold/VolumeCylinderTest.java
+++ Khulan Enkhbold/Classwork02052019_KhulanEnkhbold/VolumeCylinderTest.java
@@ -14,6 +14,6 @@ public class VolumeCylinderTest
       VolumeCylinder c1 = new VolumeCylinder();
       assertEquals(3.14, c1.calcVolumeCylinder(1.0, 1.0), 0.1);
       assertEquals(84.82, c1.calcVolumeCylinder(3.0, 3.0), 0.1);
-      assertEquals(471.23, c1.calcVolumeCylinder(5.0, 6.0), 0.1);
+      assertEquals(471.0, c1.calcVolumeCylinder(5.0, 6.0), 0.1);
     }
 }
