40016a4c5e385eded62d1c5986cc1e4e7b68fc38
Dalai Ankhbayar <Dalai@Khs-MacBook-Pro-2.local>
2019-05-03 12:39:44 +0800

    something

--- Dalai/Classwork17042019_Dalai/April17_Dalai_ClassworkJava/ClassworkThree.java
+++ Dalai/Classwork17042019_Dalai/April17_Dalai_ClassworkJava/ClassworkThree.java
@@ -14,6 +14,11 @@ public class ClassworkThree
     
     public int loop()
     {
+        int sum = 0;
         for(int i=1; i<= 10; i++)
+        {
+            sum = sum + i;
+        }
+        return sum;
     }
 }
--- Dalai/Classwork17042019_Dalai/April17_Dalai_ClassworkJava/ClassworkThreeTest.java
+++ Dalai/Classwork17042019_Dalai/April17_Dalai_ClassworkJava/ClassworkThreeTest.java
@@ -7,30 +7,10 @@ import org.junit.Test;
 
 public class ClassworkThreeTest
 {
-    /**
-     * Default constructor for test class ClassworkThreeTest
-     */
-    public ClassworkThreeTest()
+    @Test
+    public void testForLoop()
     {
-    }
-
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
-    }
-
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @After
-    public void tearDown()
-    {
+        ClassworkThree cc = new ClassworkThree(8);
+        assertEquals(55, cc.loop());
     }
 }
