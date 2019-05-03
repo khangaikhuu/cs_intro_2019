d1a68f12aef92d2ce379e7463ad9b500577b7349
G12 <g12@asu.local>
2019-05-03 13:18:30 +0800

    Classwork01052019_Myanganbayar

---
+++ Myanganbayar Nyamdavaa/Classwork01052019_Myanganbayar/Wednesday 01052019_Myanganbayar/Exercise8.java
@@ -0,0 +1,18 @@
+
+
+public class Exercise8
+{
+    public int a;
+    public int b;
+    public int Exercise8(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
