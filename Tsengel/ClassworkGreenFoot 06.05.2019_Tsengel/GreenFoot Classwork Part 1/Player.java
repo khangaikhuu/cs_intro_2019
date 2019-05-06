a9bca38e9a5dee35dc177f5a76fbf3b3ecefbe16
G12 <g12@asu.local>
2019-05-06 13:24:45 +0800

    tg

---
+++ Tsengel/ClassworkGreenFoot 06.05.2019_Tsengel/GreenFoot Classwork Part 1/Player.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Player extends Actor
+{
+    public void act() 
+    {
+        if(Greenfoot.isKeyDown("left")) {
+        turn(12);
+    }
+     if(Greenfoot.isKeyDown("right")) {
+        turn(-12);
+    }
+        move(2);
+    }    
+}
