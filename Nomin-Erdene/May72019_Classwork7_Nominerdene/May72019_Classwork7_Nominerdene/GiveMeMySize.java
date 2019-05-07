13cba9ae8e5c93c870b186825b747e1caf445ab3
G12 <g12@asu.local>
2019-05-07 13:56:43 +0800

    work

---
+++ Nomin-Erdene/May72019_Classwork7_Nominerdene/May72019_Classwork7_Nominerdene/GiveMeMySize.java
@@ -0,0 +1,15 @@
+
+
+public class GiveMeMySize
+{
+    private int [] myArray1 = {1, 2, 3, 4, 19, 25, 23, 45, 12, 58, 14};
+    
+    public int getGiveMeMySize1()
+    {
+        return myArray1.length;
+    }
+    public int getSize(int []array)
+    {
+        return array.length;
+    } 
+}
