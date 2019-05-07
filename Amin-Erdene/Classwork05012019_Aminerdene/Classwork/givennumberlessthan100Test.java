fe2bae6b6197da3e66374fe7e541ad7dcced8248
G12 <g12@asu.local>
2019-05-07 12:31:29 +0800

    aminerdene

---
+++ Amin-Erdene/Classwork05012019_Aminerdene/Classwork/givennumberlessthan100Test.java
@@ -0,0 +1,14 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+public class givennumberlessthan100Test
+{
+  @Test
+  public void testgivennumberlessthan100();
+  asssertEquals(true, q1.check100(12,1));
+  asssertEquals(true, q1.check100(131,1));
+  asssertEquals(true, q1.check100(25,1));
+}
