be6dc208588dd7c7290079a41ccd5993500e2e05
G12 <g12@asu.local>
2019-05-08 12:28:22 +0800

    adding 26.04 and 01.05 aminerdene

--- Amin-Erdene/Classwork05012019_Aminerdene/Classwork/givennumberlessthan100Test.java
+++ Amin-Erdene/Classwork05012019_Aminerdene/Classwork/givennumberlessthan100Test.java
@@ -7,8 +7,12 @@ import org.junit.Test;
 public class givennumberlessthan100Test
 {
   @Test
-  public void testgivennumberlessthan100();
-  asssertEquals(true, q1.check100(12,1));
-  asssertEquals(true, q1.check100(131,1));
-  asssertEquals(true, q1.check100(25,1));
+  public void testgivennumberlessthan100()
+  {
+   givennumberlessthan100 q1 = new givennumberlessthan100()   
+   asssertEquals(true, q1.check100(12,1));
+   asssertEquals(false, q1.check100(101,1));
+   asssertEquals(true, q1.check100(7,3));
+  }
+  
 }
