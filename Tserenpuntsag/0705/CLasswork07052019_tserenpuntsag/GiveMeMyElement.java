e141ee28993f97b2c6e02fa5ba7e093d1d3fd74c
G12 <g12@asu.local>
2019-05-08 13:37:06 +0800

    0705

--- Tserenpuntsag/0705/CLasswork07052019_tserenpuntsag/GiveMeMyElement.java
+++ Tserenpuntsag/0705/CLasswork07052019_tserenpuntsag/GiveMeMyElement.java
@@ -1,9 +1,24 @@
 
 public class GiveMeMyElement
 {
-   private int [] MyArray = (1, 2, 3, 4, 19, 25, 23, 45, 12, 58, 14)
+   private int [] myArray = {1,2, 3, 4, 19, 25, 23, 45, 12, 58, 14};
    private double [] myDoubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6};
-   private int
+   public int getSizeOfmyArray1()
+   {
+       return myArray.length;
+    }
+   public int getSizeOfmyDoubleArray()
+   {
+       return myDoubleArray.length;
+    }
+   public int getElement()
+   {
+       return myArray[2];
+    }
+   public double getElement2()
+   {
+       return myDoubleArray[2];
+    }
    
    
    
