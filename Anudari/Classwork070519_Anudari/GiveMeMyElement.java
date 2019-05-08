c845f1416dcae4d7372ec3c089f012bc0b5db295
G9 <g9@asu.local>
2019-05-08 13:53:41 +0800

    H

---
+++ Anudari/Classwork070519_Anudari/GiveMeMyElement.java
@@ -0,0 +1,12 @@
+
+public class GiveMeMyElement
+{
+   private int[] myArray = {1,2,3,4,19,25,23,45,12,58,14};
+   
+   public int getElement(int index)
+   {
+      return myArray[index];
+    }
+
+    }
+
