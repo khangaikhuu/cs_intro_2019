1c0ea39d2fc61783847ae942d607a4d4c80f3681
G12 <g12@asu.local>
2019-05-09 13:17:15 +0800

    add

---
+++ Ariunsuren/5.1/classwork/TwoNumbersAreEqua.java
@@ -0,0 +1,20 @@
+
+
+public class TwoNumbersAreEqua
+{
+   public boolean Equal(int a, int b)
+   {
+       if (a < b)
+       {
+           return false;
+        }
+       if (a > b)
+       {
+           return false;
+        }
+       else
+       {
+           return true;
+        }
+}
+}
