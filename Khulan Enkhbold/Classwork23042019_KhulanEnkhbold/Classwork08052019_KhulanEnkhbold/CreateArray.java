004a5dec4505e85aa59df29965118675c91ae541
G12 <g12@asu.local>
2019-05-14 14:49:13 +0900

    Classwork08052019_KhulanEnkhbold

---
+++ Khulan Enkhbold/Classwork23042019_KhulanEnkhbold/Classwork08052019_KhulanEnkhbold/CreateArray.java
@@ -0,0 +1,15 @@
+package Classwork08052019_KhulanEnkhbold;
+
+
+public class CreateArray
+{
+   private int myArray [] = new int [7];
+   public void CreateArray (int index, int number)
+   {
+       myArray [index] = number;
+    }
+    public int getElement (int index)
+    {
+        return myArray [index];
+    }
+}
