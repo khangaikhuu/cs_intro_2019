e845e8fa2adbfa28a0b2b585298be3e18956e578
G12 <g12@asu.local>
2019-05-24 13:20:30 +0800

    unittest tsengel

---
+++ Tsengel/Classwork 08.05.2019_Tsengel/JavaQuestions/Q9Test.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Q9Test
+{
+   @Test
+  public void Q9Test()
+  {
+      Q9 q1 = new Q9();
+      assertEquals(false, q1.Q9(true,false));
+      assertEquals(true, q1.Q9(true,true));
+      assertEquals(false, q1.Q9(false,true));
+    }
+}
